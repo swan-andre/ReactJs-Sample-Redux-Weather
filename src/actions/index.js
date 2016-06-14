2ad31409bb9791d474ed355c6007b4f09288ce39
Swan <swan.andre01@gmail.com>
2016-06-14 17:23:43 +0200

    End of tut

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = '91a84850b83748350ed7aad85df28e42';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},fr`;
+    const request = axios.get(url);
+
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
